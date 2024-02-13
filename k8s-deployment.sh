8b87a3018c43c3a50f58ebd3f313ccd8984efc05
ne_arauf <abdul.rauf@netefficiency.co.uk>
2024-02-13 18:26:29 +0000

    mounted volums and fixes for tmp  directory

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -15,6 +15,3 @@ else
     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
 fi
 ############### k8s-deployment.sh ###############
-
-
-kubectl -n default apply -f k8s_deployment_service.yaml
