e31c4d22fae362d3f17782d192893edeab78f27a
ne_arauf <abdul.rauf@netefficiency.co.uk>
2024-02-15 12:39:02 +0000

    Revert "no message"
    This reverts commit 093764e64da01aec3e5c54e567f8c87fc6ae6a87.

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -44,7 +44,7 @@ public class NumericController {
 			return Integer.parseInt(response);
 		}
 		
-		@GetMapping("/error/50")
+		@GetMapping("/error")
 		public int increment(@PathVariable int value) {
 			return "There is been an error";
 		}
