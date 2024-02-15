6222fb84c0f5d414e2de72a2866453770c0be493
ne_arauf <abdul.rauf@netefficiency.co.uk>
2024-02-15 15:09:56 +0000

    syntax error fixed

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -1,13 +1,3 @@
-############## Add spring-boot-starter-security dependency in pom.xml ############## 
-<dependency>
-   <groupId>org.springframework.boot</groupId>
-   <artifactId>spring-boot-starter-security</artifactId>
-</dependency>
-############## Add spring-boot-starter-security dependency in pom.xml ############## 
-
-
-
-############## Create a new class - src/main/java/com/devsecops/WebSecurityConfig.java ############## 
 package com.devsecops;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,8 +13,3 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 }
-############## Create a new class - /src/main/java/com/devsecops/WebSecurityConfig.java ############## 
-
-
-
-Modify the CVSS value as shown in video 
